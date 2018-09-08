401dde6bbc24a70d8a5209b2ea070305344827d1
Fergus Taylor <fergustay@gmail.com>
2018-09-08 21:09:54 +0100

    formatting

--- 50 Drugs/js/index.js
+++ 50 Drugs/js/index.js
@@ -195,56 +195,56 @@ span.appendChild(breaks);
 
 var example = document.createElement('p');
 example.setAttribute("class", "example")
-var example5 = document.createTextNode(item["Example(s) of drugs:"]);
+var example5 = document.createTextNode("Example(s) of drugs: "+item["Example(s) of drugs:"]);
 example.appendChild(example5);
 span.appendChild(example);
 span.appendChild(breaks);
 
 var pcpk = document.createElement('p');
 pcpk.setAttribute("class", "pcpk")
-var pcpk5 = document.createTextNode(item["Important pharmacokinetics / pharmacodynamics:"]);
+var pcpk5 = document.createTextNode("Important PK/PD: "+item["Important pharmacokinetics / pharmacodynamics:"]);
 pcpk.appendChild(pcpk5);
 span.appendChild(pcpk);
 span.appendChild(breaks);
 
 var indication = document.createElement('p');
 indication.setAttribute("class", "indication")
-var indication5 = document.createTextNode(item["Indication(s):"]);
+var indication5 = document.createTextNode("Indication(s): "+item["Indication(s):"]);
 indication.appendChild(indication5);
 span.appendChild(indication);
 span.appendChild(breaks);
 
 var moa = document.createElement('p');
 moa.setAttribute("class", "moa")
-var moa5 = document.createTextNode(item["Mechanism of action:"]);
+var moa5 = document.createTextNode("Mechanism of action: "+item["Mechanism of action:"]);
 moa.appendChild(moa5);
 span.appendChild(moa);
 span.appendChild(breaks);
 
 var otherinfo = document.createElement('p');
 otherinfo.setAttribute("class", "otherinfo")
-var otherinfo5 = document.createTextNode(item["Other information:"]);
+var otherinfo5 = document.createTextNode("Other information: "+item["Other information:"]);
 otherinfo.appendChild(otherinfo5);
 span.appendChild(otherinfo);
 span.appendChild(breaks);
 
 var patientinfo = document.createElement('p');
 patientinfo.setAttribute("class", "patientinfo")
-var patientinfo5 = document.createTextNode(item["Patient information:"]);
+var patientinfo5 = document.createTextNode("Patient information: "+item["Patient information:"]);
 patientinfo.appendChild(patientinfo5);
 span.appendChild(patientinfo);
 span.appendChild(breaks);
 
 var sideeffects = document.createElement('p');
 sideeffects.setAttribute("class", "sideeffects")
-var sideeffects5 = document.createTextNode(item["Side effects:"]);
+var sideeffects5 = document.createTextNode("Side effects: "+item["Side effects:"]);
 sideeffects.appendChild(sideeffects5);
 span.appendChild(sideeffects);
 span.appendChild(breaks);
 
 var classinfo = document.createElement('p');
 classinfo.setAttribute("class", "classinfo")
-var classinfo5 = document.createTextNode(item.class);
+var classinfo5 = document.createTextNode("Drug class: "+item.class);
 classinfo.appendChild(classinfo5);
 span.appendChild(classinfo);
 span.appendChild(breaks);
